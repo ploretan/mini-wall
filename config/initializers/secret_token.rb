950ceabcbbeb7473d6ec619254d0809b3f1a37b2
ploretan <peter.loretan@gmail.com>
2015-06-14 04:25:28 -0700

    base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniWall::Application.config.secret_key_base = '1e680e906fa4cdbd659f8788db04dedadcc2a69bcbbdd8ebf00066acbfd9d09af7a80f68dea415c816c5e33ee43408776a96c4e22b90337c8e67a24c59c1f830'
